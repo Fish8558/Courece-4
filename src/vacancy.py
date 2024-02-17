b47c0b4fb54fd988d49dd64572da18149eda06b4
Stas <stas_sever@mail.ru>
2024-02-18 01:08:56 +0300

    added sorting methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -71,3 +71,121 @@ class Vacancy:
     def __gt__(self, other) -> bool:
         """Метод сравнения вакансий"""
         return self.value > other.value
+
+    @staticmethod
+    def filter_vacancies(list_vacancy: list, filters: list) -> list:
+        """
+        Метод для фильтрации списка по ключевым словам
+        :param list_vacancy: список вакансий для фильтрации
+        :param filters: список ключевых слов для фильтрации
+        :return: возвращает отфильтрованный список вакансий
+        """
+        if not filters:
+            return list_vacancy
+
+        filtered_vacancies = []
+        for item in list_vacancy:
+            description = item.description.lower()
+            if all(word in description for word in filters):
+                filtered_vacancies.append(item)
+        return filtered_vacancies
+
+    @staticmethod
+    def sort_vacancies(list_vacancy: list) -> list:
+        """
+        Метод сортировки вакансий по зарплате
+        :param list_vacancy: Список вакансий для сортировки
+        :return: возвращает отсортированный список
+        """
+        return sorted(list_vacancy, reverse=True)
+
+    @staticmethod
+    def get_top_vacancies(list_vacancy: list, num: str) -> list:
+        """
+        Метод получения топ вакансий
+        :param list_vacancy: список вакансий
+        :param num: количество вакансий которое необходимо вернуть
+        :return: возвращает список топ вакансий
+        """
+        if num == "":
+            num = len(list_vacancy)
+        elif num.isdigit():
+            num = int(num)
+        if 0 < len(list_vacancy) <= num:
+            return list_vacancy
+        else:
+            return list_vacancy[0:num]
+
+    @staticmethod
+    def print_vacancies(list_vacancy: list) -> None:
+        """
+        Метод вывода вакансий
+        :param list_vacancy: список вакансий для вывода
+        """
+        for index, item in enumerate(list_vacancy, 1):
+            print(f"Вакансия №{index}:\n"
+                  f"{item}")
+
+    @staticmethod
+    def filter_name_vacancy(list_vacancy: list, user_answer: str) -> None:
+        index = 0
+        for item in list_vacancy:
+            if user_answer in item.name.lower():
+                index += 1
+                print(f"Вакансия №{index}:\n"
+                      f"{item}")
+        if index == 0:
+            print("\nПодходящих вакансий не найдено. Попробуйте изменить запрос")
+        else:
+            print(f"Всего вакансий получено: {index}")
+
+    @staticmethod
+    def filter_name_city(list_vacancy: list, user_answer: str) -> None:
+        index = 0
+        for item in list_vacancy:
+            if user_answer in item.area.lower():
+                index += 1
+                print(f"Вакансия №{index}:\n"
+                      f"{item}")
+        if index == 0:
+            print("\nПодходящих вакансий не найдено. Попробуйте изменить запрос")
+        else:
+            print(f"Всего вакансий получено: {index}")
+
+    @staticmethod
+    def filter_name_company(list_vacancy: list, user_answer: str) -> None:
+        index = 0
+        for item in list_vacancy:
+            if user_answer == item.employer.lower():
+                index += 1
+                print(f"Вакансия №{index}:\n"
+                      f"{item}")
+        if index == 0:
+            print("\nПодходящих вакансий не найдено. Попробуйте изменить запрос")
+        else:
+            print(f"Всего вакансий получено: {index}")
+
+    @staticmethod
+    def filter_user_salary(list_vacancy: list, user_from: int, user_to: int) -> None:
+        index = 0
+        for item in list_vacancy:
+            if item.salary:
+                if item.from_salary and item.to_salary:
+                    if item.from_salary == user_from and item.to_salary == user_to:
+                        index += 1
+                        print(f"Вакансия №{index}:\n"
+                              f"{item}")
+                elif item.from_salary and not item.to_salary:
+                    if item.from_salary in range(user_from, user_to + 1):
+                        index += 1
+                        print(f"Вакансия №{index}:\n"
+                              f"{item}")
+                elif not item.from_salary and item.to_salary:
+                    if item.to_salary in range(user_from, user_to + 1):
+                        index += 1
+                        print(f"Вакансия №{index}:\n"
+                              f"{item}")
+        if index == 0:
+            print("\nПодходящих вакансий не найдено. Попробуйте изменить запрос")
+        else:
+            print(f"Всего вакансий получено: {index}")
