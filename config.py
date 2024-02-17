4787ed0880fdaf83e621d66605e17f18569647cd
Stas <stas_sever@mail.ru>
2024-02-17 23:45:58 +0300

    added project path

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+PATH_FOLDER = os.path.join(ROOT_DIR, 'data')
+JSON_PATH_ALL_VACANCY = os.path.join(PATH_FOLDER, 'all_job_vacancies.json')
+JSON_PATH_FILTER_VACANCY = os.path.join(PATH_FOLDER, 'filter_job_vacancies.json')
